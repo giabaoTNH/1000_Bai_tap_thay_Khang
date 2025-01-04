83463e5cf8cc6c762ad8459cbc5b404bfb201862
giabaoTNH <hanguyengiabao1234@gmail.com>
2025-01-04 14:18:36 +0700

    bai tap

--- 111.cpp
+++ 111.cpp
@@ -6,5 +6,30 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n;cin >> n;
+	cout << "A.\n";
+	int d = n-1;
+	for (int i = 1; i<=n; i++) {
+		for (int j = 1; j<=d; j++) cout << ' ';
+		for (int j = 1; j<=i*2-1; j++) cout << '*';
+		d--;
+		cout << endl;
+	}
+	cout << "B.\n";
+	d = n-2;
+	for (int i = 1; i<=n-1; i++) cout << ' ';
+	cout << '*' <<'\n';
+	for (int i = 2; i<=n-1; i++) {
+		for (int j = 1; j<=d; j++) cout << ' ';
+		cout << '*';
+		for (int j = 2; j<i*2-1; j++) cout << ' ';
+		cout << '*';
+		d--;
+		cout << endl;
+	}
+	for (int i = 1; i<=n*2-1; i++) cout << '*';
+	cout << endl;
+	
+
 	return 0;
 }
